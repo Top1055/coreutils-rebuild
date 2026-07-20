47b4a4dd9ac93c9e2e4bb3358eef189707bf2a07
Top1055 <123alexfeetham@gmail.com>
2026-07-20 23:50:27 +0100

    first program implemented, refresher of main function

---
+++ 01-echoargs/echoargs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    fprintf(stderr, "Usage: %s <args...>\n", argv[0]);
+    return 1;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    printf("%i. %s\n", i, argv[i]);
+  }
+  return 0;
+}
